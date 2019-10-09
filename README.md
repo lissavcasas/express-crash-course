d7ebe3a8b52bff443aca6ee8c329b25f7102de90
marimeli <marimel.casas@gmail.com>
2019-10-09 16:43:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# express-crash-course
+Using express
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1></h1>Hello from Node! :) </h1>');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+
+
+
+
